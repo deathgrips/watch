b599e5722036bd5d33f25c73339b293cfac5d224
deathgrips <farmakon91@gmail.com>
2017-01-16 17:04:24 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+;(function($){
+	"use strict";
+ 
+	$(function(){
+		$('.ba-slider').slick({
+			dots: true,
+			vertical: true,
+			verticalSwiping: true,
+			infinite: false
+		});
+	})
+
+})(jQuery);
